5b3d3bf1a00e4ebf1b618afe4347e1e4b904efa7
lshapz <laurashapiro01@gmail.com>
2016-12-22 17:26:01 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+  max = 0
+  (100..999).each do |one|
+    (100..999).each do |two|
+      prod = one * two
+      if pal?(prod) && prod > max
+        max = prod
+      end
+    end
+  end
+  max
+end 
+
+def pal?(input)
+  input.to_s == input.to_s.reverse
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+
+def answer
+  max = 0
+  (100..999).each do |one|
+    (100..999).each do |two|
+      prod = one * two
+      if self.pal?(prod) && prod > max
+        max = prod
+      end
+    end
+  end
+  max
+end 
+
+def pal?(input)
+  input.to_s == input.to_s.reverse
+end
+
+end
